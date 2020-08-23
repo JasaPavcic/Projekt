72c6c3f7b0d211be05d97c55833aaac40c523f06
Jaša Pavčič/usr/local/bin/python3 <jasa.pavcic15@gmail.com>
2020-08-23 14:27:24 +0200

    Izpis kart

--- blackjack.py
+++ blackjack.py
@@ -1,68 +1,71 @@
 import random
 import os
 
+
+def izracunaj_vsoto(roka):
+    vsota = 0
+
+    ne_as = [karta for karta in roka if karta != 'As' or 'Ak' or 'Ap' or 'Aka']
+    As = [karta for karta in roka if karta == 'As' or 'Ak' or 'Ap' or 'Aka']
+
+    for karta in ne_as:
+        if karta in 'JQK':
+            vsota += 10
+        else:
+            vsota += int(karta)
+
+    for karta in As:
+        if vsota <= 10:
+            vsota += 11
+        else:
+            sum +=1
+
+    return vsota
+
+
+
 # vseh kart je 52
 karte = [
-    '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
-    '2K', '3K', '4K', '5K', '6K', '7K', '8K', '9K', '10K', 'JK', 'QK', 'KK', 'AK',
-    '2P', '3P', '4P', '5P', '6P', '7P', '8P', '9P', '10P', 'JP', 'QP', 'KP', 'AP',
-    '2Ka', '3Ka', '4Ka', '5Ka', '6Ka', '7Ka', '8Ka', '9Ka', '10Ka', 'JKa', 'QKa', 'KKa', 'AKa',
+    '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks', 'As',
+    '2k', '3k', '4k', '5k', '6k', '7k', '8k', '9k', '10k', 'Jk', 'Qk', 'Kk', 'Ak',
+    '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', 'Jp', 'Qp', 'Kp', 'Ap',
+    '2ka', '3ka', '4ka', '5ka', '6ka', '7ka', '8ka', '9ka', '10ka', 'Jka', 'Qka', 'Kka', 'Aka',
 
 ]
 
 nove_karte = [
-    '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', '10S', 'JS', 'QS', 'KS', 'AS',
-    '2K', '3K', '4K', '5K', '6K', '7K', '8K', '9K', '10K', 'JK', 'QK', 'KK', 'AK',
-    '2P', '3P', '4P', '5P', '6P', '7P', '8P', '9P', '10P', 'JP', 'QP', 'KP', 'AP',
-    '2Ka', '3Ka', '4Ka', '5Ka', '6Ka', '7Ka', '8Ka', '9Ka', '10Ka', 'JKa', 'QKa', 'KKa', 'AKa',
+    '2s', '3s', '4s', '5s', '6s', '7s', '8s', '9s', '10s', 'Js', 'Qs', 'Ks', 'As',
+    '2k', '3k', '4k', '5k', '6k', '7k', '8k', '9k', '10k', 'Jk', 'Qk', 'Kk', 'Ak',
+    '2p', '3p', '4p', '5p', '6p', '7p', '8p', '9p', '10p', 'Jp', 'Qp', 'Kp', 'Ap',
+    '2ka', '3ka', '4ka', '5ka', '6ka', '7ka', '8ka', '9ka', '10ka', 'Jka', 'Qka', 'Kka', 'Aka',
 ]
 
 random.shuffle(karte)
 
 
 # vsak ima svojo vsoto denarja od katere se odšteva
-a = 50
-b = 50
-c = 50
-
-denarnica_igralec = a
-denarnica_igralec_2 = b
-denarnica_igralec_3 = c
 
 
 diler = []
 igralec = []
-igralec_2 = []
-igralec_3 = []
-kos = []
+
 
 
 # dokler je poln seznam prvih kart
 # while len(karte) > 0:
+
 igralec.append(karte.pop())
-igralec_2.append(karte.pop())
-igralec_3.append(karte.pop())
 diler.append(karte.pop())
 igralec.append(karte.pop())
-igralec_2.append(karte.pop())
-igralec_3.append(karte.pop())
 diler.append(karte.pop())
-# to še pogruntaj nekak
-    # igralec.append(nove_karte.pop())
-    # igralec_2.append(nove_karte.pop())
-    # igralec_3.append(nove_karte.pop())
-    # delilec.append(nove_karte.pop())
-    # igralec.append(nove_karte.pop())
-    # igralec_2.append(nove_karte.pop())
-    # igralec_3.append(nove_karte.pop())
-    # delilec.append(nove_karte.pop())
+
+
+# TUKAJ SE MORAS UPOSTEVATI DRUGE IGRALCE
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
-
-    print('Dilerjeve karte: [{}][?]'.format(diler[0]))
-    print('Igralčeve karte: [{}] ({})'.format(']['.join(igralec), 000)
-    print('Igralčeve karte: [{}] ({})'.format(']['.join(igralec_2), 000)
-    print('Igralčeve karte: [{}] ({})'.format(']['.join(igralec_3), 000)
     
+    print('Dilerjeve karte: [{}][?]'.format(diler[0]))
+    print('Igralceve karte' + str(igralec))
+
     break
